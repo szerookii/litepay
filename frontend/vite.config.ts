223f8748e0121d9ef638a66922b730d32d0c2ca7
large <contact@szeroki.fr>
2026-04-23 11:52:18 +0200

    chore: update frontend config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,17 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
+	],
 	server: {
 		proxy: {
-			"/api": {
-				target: "http://127.0.0.1:8080",
-				changeOrigin: true,
-				secure: false,
-				ws: true
-			}
+			'/api': 'http://localhost:8080'
 		}
 	}
 });
